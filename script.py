408413a50077cf4bbedb589b0fe157924d72188d
kaosar100 <46788194+kaosar100@users.noreply.github.com>
2023-09-12 00:22:11 +0600

    Update script.py

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 counter = 0
 face_match = False
-ref_image = cv2.imread("My Photo.jpg")
+ref_image = cv2.imread("My Photo.jpg") #put your image path here
 
 cv2.imshow("check", ref_image)
 
